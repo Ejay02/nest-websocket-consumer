bcf98b7e9274e9a498d80a876bbf8ba22cc403be
Ejay02 <e.jae02@gmail.com>
2022-11-24 17:02:32 +0100

    nestjs websocket consumer

---
+++ src/socket/socket-client.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { io, Socket } from 'socket.io-client';
+
+@Injectable()
+export class SocketClient implements OnModuleInit {
+  // create socket io connection
+
+  public socketClient: Socket;
+
+  constructor() {
+    this.socketClient = io('http://localhost:9001');
+  }
+
+  onModuleInit() {
+    this.registerConsumerEvents();
+  }
+
+  private registerConsumerEvents() {
+    this.socketClient.emit('newMessage', { msg: 'hey there' });
+    this.socketClient.on('connect', () => {
+      console.log('Connected to Gateway');
+    });
+    this.socketClient.on('onMessage', (payload: any) => {
+      console.log(payload);
+    });
+  }
+}
